45dfd17c5485e389d5d3b4a3d1990203c065154c
marmeladema <xademax@gmail.com>
2019-08-31 11:21:43 +0100

    Compile C code with AddressSanitizer enabled

--- build.rs
+++ build.rs
@@ -4,8 +4,10 @@ fn main() {
     cc::Build::new()
         .include("include")
         .file("src/tests.c")
+        .flag("-fsanitize=address")
         .warnings(true)
         .extra_warnings(true)
         .warnings_into_errors(true)
         .compile("ctests");
+    println!("cargo:rustc-link-lib=asan");
 }
